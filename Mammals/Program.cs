b544b7a92ec2012aa62b610f94d179038f55bc9b
Lindsey White <LindseyWhite@RocketMortgage.com>
2022-08-22 17:12:38 -0400

    Lists of objects and inheritance demos

---
+++ Mammals/Program.cs
@@ -0,0 +1,56 @@
+﻿Dog first = new Dog() { Name = "Fido", HairColor = "Black", LegCount = 4, BarkVolume = 8 };
+Dog second = new Dog() { Name = "Nellie", HairColor = "Yellow", LegCount = 4, BarkVolume = 5 };
+Orangutan third = new Orangutan() { Name = "Fred", HairColor = "Orange", LegCount = 2, ThumbLength = 3 };
+
+List<Mammal> pets = new List<Mammal>();
+pets.Add(first);
+pets.Add(second);
+pets.Add(third);
+
+foreach (Mammal pet in pets)
+{
+    string info = pet.ToString();
+    Console.WriteLine(pet);
+}
+
+class Mammal
+{
+    public string Name;
+    public string HairColor;
+    public int LegCount;
+    
+    public void Walk()
+    {
+        Console.WriteLine("I am walking");
+    }
+
+    // We are overriding what's called a "virtual" function. 
+    // i.s. a "virtual" function is one that you can override. 
+    public override string ToString()
+    {
+        return "This is a mammal";
+    }
+}
+
+//parent class listed after the derived class separated by colon
+class Dog : Mammal
+{
+    public int BarkVolume;
+
+    public override string ToString()
+    {
+        return $"Dog named {Name}, hair {HairColor}, {LegCount} legs, barks at volume {BarkVolume}";
+    }
+
+}
+
+class Orangutan : Mammal
+{
+    public int ThumbLength;
+
+    public override string ToString()
+    {
+        return $"Orangutan named {Name}, hair {HairColor}, {LegCount} legs, thumb is {ThumbLength} inches";
+    }
+
+}
---
+++ Vehicles/Program.cs
@@ -0,0 +1,68 @@
+﻿Sedan mycar = new Sedan(4, "Blue", true, 4);
+
+
+RaceCar myracecar = new RaceCar(4, "Red", 400);
+
+
+// Polymorphism - calling the correct version of the method 
+Vehicle something;
+something = mycar;
+something.Drive();
+
+something = myracecar;
+something.Drive();
+
+
+
+class Vehicle
+{
+    public int WheelCount;
+    public string Color;
+
+    public Vehicle(int _wheelCount, string _color)
+    {
+        WheelCount = _wheelCount;
+        Color = _color;
+    }
+
+    //virtual means you can override it in derived classes
+    public virtual void Drive()
+    {
+        Console.WriteLine("I am driving");
+    }
+}
+
+class Sedan : Vehicle
+{
+    public bool HasHatchBack;
+    public int DoorCount;
+
+    public Sedan (int _SedanWheelCount, string _SedanColor, bool _hasHatchBack, int _doorCount) 
+        : base (_SedanWheelCount, _SedanColor)
+    {
+        HasHatchBack = _hasHatchBack;
+        DoorCount = _doorCount;
+    }
+
+    public override void Drive()
+    {
+        Console.WriteLine("I am driving speed limit!");
+    }
+
+}
+
+class RaceCar : Vehicle
+{
+    public int EngineSize;
+
+    public RaceCar(int _WheelCount, string _Color, int _EngineSize)
+        : base(_WheelCount, _Color)
+    {
+        EngineSize = _EngineSize;
+    }
+
+    public override void Drive()
+    {
+        Console.WriteLine("I am driving really fast!");
+    }
+}
